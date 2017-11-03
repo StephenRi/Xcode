6207b90ea918b2584f076f187743bda22772157a
赵帅兵 <1191850012@qq.com>
2017-11-04 03:06:54 +0800

    add hello

---
+++ LeetCode/Hello-LeetCode/Hello-LeetCode/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Hello-LeetCode
+//
+//  Created by Shuaibing_Zhao on 2017/11/4.
+//  Copyright © 2017年 Shuaibing_Zhao. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, LeetCode!\n";
+    return 0;
+}
