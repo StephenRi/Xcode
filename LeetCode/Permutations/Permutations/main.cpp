5f8743173565e68907f99620700da76dca8f63c2
Shuaibing_Zhao <zhaoshb@nbjl.nankai.edu.cn>
2017-11-06 01:28:57 +0800

    add c++ document

--- LeetCode/Permutations/Permutations/main.cpp
+++ LeetCode/Permutations/Permutations/main.cpp
@@ -7,11 +7,33 @@
 //
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+vector<vector<int>> permute(vector<int>& nums) {
+    vector<vector<int>> answer;
+    vector<int> subAnswer;
+    
+    return answer;
+}
+
+void dfs(vector<int> & nums, vector<vector<int>> & answer, vector<int> subAnswer, int level)
+{
+    
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    cout << "Hello, World!\n";
+    vector<int> input;
+    cout << "该程序给出一个整数数组的全排列" << endl;
+    cout << "请输入您的数组（以#结束）： ";
+    int tmp;
+    while(cin >> tmp)
+    {
+        input.push_back(tmp);
+    }
+    vector<vector<int>> output = permute(input);
+    
     return 0;
 }
