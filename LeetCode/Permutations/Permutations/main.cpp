e79793ded571ed389bb9939d32173f5bb1385177
Shuaibing_Zhao <zhaoshb@nbjl.nankai.edu.cn>
2017-11-05 21:46:49 +0800

    add Permutations

---
+++ LeetCode/Permutations/Permutations/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Permutations
+//
+//  Created by Shuaibing_Zhao on 2017/11/5.
+//  Copyright © 2017年 Shuaibing_Zhao. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
